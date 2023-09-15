40660a0e424e24539d978ae6eaffc002eea0d82f
tanvirsiraj <tanvirbinsiraj.edu.bd@gmail.com>
2023-09-15 21:52:11 +0600

    google font and header component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Header from "./Header/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <main></main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold">Course Registration</h1>
+    </div>
+  );
+};
+
+export default Header;
